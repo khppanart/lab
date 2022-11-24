307b76b9219ded8e2b0025552c9b1f33c7b62b06
khppanart <6521061003@cdti.ac.th>
2022-11-24 15:56:47 +0700

    first commit

---
+++ lab2-1.ino
@@ -0,0 +1,36 @@
+int led1 = 17;
+int led2 = 2;
+int led3 = 15;
+int led4 = 12;
+
+
+void setup() {
+  pinMode (led1,OUTPUT);
+  pinMode (led2,OUTPUT);
+  pinMode (led3,OUTPUT);
+  pinMode (led4,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (led1,HIGH);
+  delay(1000);
+  digitalWrite (led1,LOW);
+  delay(500);
+
+  digitalWrite (led2,HIGH);
+  delay(1000);
+  digitalWrite (led2,LOW);
+  delay(500);
+
+  digitalWrite (led3,HIGH);
+  delay(1000);
+  digitalWrite (led3,LOW);
+  delay(500);
+
+  digitalWrite (led4,HIGH);
+  delay(1000);
+  digitalWrite (led4,LOW);
+  delay(500);
+
+}
